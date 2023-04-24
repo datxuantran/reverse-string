84806519eb1e4832ed02e115d0a9991d082b28a8
Dat Xuan Tran <trandat.student@gmail.com>
2023-04-24 16:27:06 +0200

    implementing

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # reverse-string
+
+```bash
+node --print "require('./').reverse('Hello Dat\!')"
+node --print "require('./').hey"
+```
---
+++ index.js
@@ -0,0 +1,10 @@
+function reverse(sentence){
+    const words_array = sentence.split(" "); 
+    const reversed_words_array = words_array.reverse(); 
+    const reverse_sentence = reversed_words_array.join(" "); 
+    return reverse_sentence; 
+}
+
+const hey="Hi Mom!"; 
+
+module.exports = {hey, reverse}; 
